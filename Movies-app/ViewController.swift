5975d63b9b290d42e63557224579f7f9366d76b9
Agustin Daguerre <agustin.daguerre@moove-it.com>
2017-06-21 10:57:48 -0300

    Initial Commit

---
+++ Movies-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Movies-app
+//
+//  Created by Agustin Daguerre on 6/21/17.
+//  Copyright © 2017 agustindaguerre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
