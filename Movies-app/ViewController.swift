18412adc155b22500f36dd6b791892696e47a967
Agustin Daguerre <agustin.daguerre@moove-it.com>
2017-07-10 02:18:17 -0300

    Style fixes

--- Movies-app/ViewController.swift
+++ Movies-app/ViewController.swift
@@ -10,8 +10,24 @@ import UIKit
 import XLPagerTabStrip
 
 class ViewController: ButtonBarPagerTabStripViewController {
-
+    
     override func viewDidLoad() {
+        settings.style.buttonBarBackgroundColor = .white
+        settings.style.buttonBarItemBackgroundColor = .white
+        settings.style.selectedBarBackgroundColor = .darkGray
+        settings.style.buttonBarItemFont = .boldSystemFont(ofSize: 14)
+        settings.style.selectedBarHeight = 2.0
+        settings.style.buttonBarMinimumLineSpacing = 0
+        settings.style.buttonBarItemTitleColor = .black
+        settings.style.buttonBarItemsShouldFillAvailiableWidth = true
+        settings.style.buttonBarLeftContentInset = 0
+        settings.style.buttonBarRightContentInset = 0
+        changeCurrentIndexProgressive = { [weak self] (oldCell: ButtonBarViewCell?, newCell: ButtonBarViewCell?, progressPercentage: CGFloat, changeCurrentIndex: Bool, animated: Bool) -> Void in
+            guard changeCurrentIndex == true else { return }
+            oldCell?.label.textColor = .black
+            newCell?.label.textColor = .darkGray
+        }
+        
         super.viewDidLoad()
     }
 
